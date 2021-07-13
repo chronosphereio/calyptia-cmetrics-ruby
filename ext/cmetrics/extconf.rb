8325287899f838a3eeacc6805fb7e117718d1d2e
Hiroshi Hatake <hatake@calyptia.com>
2021-07-13 18:26:43 +0900

    Use dir.glob for lib64 installation
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- ext/cmetrics/extconf.rb
+++ ext/cmetrics/extconf.rb
@@ -35,11 +35,11 @@ class BuildCMetrics
   def build
     unless File.exist?(@checkpoint)
       @recipe.cook
-      libcmetrics_path = File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib/libcmetrics.a")
+      libcmetrics_path = Dir.glob(File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib*/libcmetrics.a")).first
       FileUtils.cp(libcmetrics_path, File.join(ROOT, "ext", "cmetrics", "libcmetrics.a"))
-      libmpack_path = File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib/libmpack.a")
+      libmpack_path = Dir.glob(File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib*/libmpack.a")).first
       FileUtils.cp(libmpack_path, File.join(ROOT, "ext", "cmetrics", "libmpack.a"))
-      libxxhash_path = File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib/libxxhash.a")
+      libxxhash_path = Dir.glob(File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/lib*/libxxhash.a")).first
       FileUtils.cp(libxxhash_path, File.join(ROOT, "ext", "cmetrics", "libxxhash.a"))
       include_path = File.join(ROOT, "ports/#{@recipe.host}/cmetrics/#{@version}/include/")
       FileUtils.cp_r(Dir.glob(File.join(include_path, "*")), File.join(ROOT, "ext", "cmetrics"))
